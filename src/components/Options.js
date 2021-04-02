4b53a33e17012c9b27565b2fe4bd9075d43d0298
sourav <souravchiku50@gmail.com>
2021-04-02 18:30:01 +0530

    2commit

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,15 +1,16 @@
 
-const Options = ({addOptions, deleteOptions,uid ,updateText})=>{
+const Options = ({addOptions, deleteOptions, uid ,updateText}) => {
     return (
-        <div className="col-md-8 offset-md2 col-12 input-group my-3">
-            <input type="text" className="form-control" placeholder="Option Text" onChange={event => {updateText(uid,event.target.value)}}>
-            </input>
-            <div className="input-group-append">
-                <button className="btn btn-outline-secondary" type="button" onClick={()=> addOptions()}>+</button>
-                <button className="btn btn-outline-secondary" type="button" onClick={()=> deleteOptions()}>-</button>
+        <>
+            <div className="col-md-8 offset-md-2 col-12 input-group my-3">
+                <input type="text" className="form-control" placeholder="Option Text" onChange={event => {updateText(uid, event.target.value)}} />
+                <div className="input-group-append">
+                    <button className="btn btn-outline-secondary" type="button" onClick={() => addOptions()}>+</button>
+                    <button className="btn btn-outline-secondary" type="button" onClick={() => deleteOptions()}>-</button>
+                </div>
             </div>
-        </div>
-    )
+        </>
+    );
 }
 
-export default Options
+export default Options;
